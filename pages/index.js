81bd1f9996c65fa71bf1dbe4124d264525116b8b
João <sonicbatataazul@gmail.com>
2026-04-30 23:55:33 -0300

    index

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,12 @@ import { useEffect, useState } from 'react'
 import Chat from '../components/Chat'
 import Head from 'next/head'
 
-function generateUserId() {
-  return 'user_' + Math.random().toString(36).slice(2, 11)
+function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+    const r = Math.random() * 16 | 0
+    const v = c === 'x' ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
 }
 
 export default function Home() {
@@ -12,7 +16,7 @@ export default function Home() {
   useEffect(() => {
     let id = sessionStorage.getItem('chatanon_uid')
     if (!id) {
-      id = generateUserId()
+      id = generateUUID()
       sessionStorage.setItem('chatanon_uid', id)
     }
     setUserId(id)
